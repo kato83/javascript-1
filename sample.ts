50e38a23982dc7df243c30bf00a5e87e68df2526
kato83 <katomeganeb83@gmail.com>
2025-08-11 16:01:50 +0900

    update sample.ts

--- sample.ts
+++ sample.ts
@@ -1,5 +1,16 @@
-console.log('sample.ts');
-
 export function sum(...numbers: number[]): number {
   return numbers.reduce((sum, x) => sum + x, 0);
+}
+
+if (import.meta.main) {
+  console.info([
+    'sample.ts 利用方法',
+    '',
+    '```javascript',
+    'import { sum } from "./sample.ts";',
+    'console.log(sum(1, 2)); // 3',
+    '````'
+  ].join('\n'));
+} else {
+  console.log('imported sample.ts');
 }
